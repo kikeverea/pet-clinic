43da06c081c29c50f97c458033d4c8955ab6c9c9
Enrique Verea <kikeverea@gmail.com>
2022-06-01 00:34:31 +0200

    Create CrudService

--- src/main/java/com/cac/learn/petclinic/services/OwnerService.java
+++ src/main/java/com/cac/learn/petclinic/services/OwnerService.java
@@ -2,12 +2,7 @@ package com.cac.learn.petclinic.services;
 
 import com.cac.learn.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
