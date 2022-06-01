a243f54c3b7d3573028f61e224084436ac41c500
Enrique Verea <kikeverea@gmail.com>
2022-06-02 00:24:10 +0200

    Create vets index page and controller

---
+++ pet-clinic-web/src/main/java/com/cac/labs/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.cac.labs.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String listVets() {
+        return "vets/index";
+    }
+
+}
