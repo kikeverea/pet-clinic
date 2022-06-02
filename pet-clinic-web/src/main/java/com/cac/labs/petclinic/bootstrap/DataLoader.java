0f5926e88524e59cf36bf7cdb8418a27106afcf1
Enrique Verea <kikeverea@gmail.com>
2022-06-02 13:12:12 +0200

    Create DataLoader for boot data loading

---
+++ pet-clinic-web/src/main/java/com/cac/labs/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,66 @@
+package com.cac.labs.petclinic.bootstrap;
+
+import com.cac.labs.petclinic.model.BaseEntity;
+import com.cac.labs.petclinic.model.Owner;
+import com.cac.labs.petclinic.model.Person;
+import com.cac.labs.petclinic.model.Vet;
+import com.cac.labs.petclinic.services.CrudService;
+import com.cac.labs.petclinic.services.OwnerService;
+import com.cac.labs.petclinic.services.VetService;
+import com.cac.labs.petclinic.services.map.OwnerServiceMap;
+import com.cac.labs.petclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        printLoaded(ownerService, "owners");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet2);
+
+        printLoaded(vetService, "vets");
+    }
+
+    private void printLoaded(CrudService<? extends Person, Long> service, String serviceName) {
+        System.out.println("Loaded " + serviceName + ":");
+        service .findAll()
+                .forEach(loaded -> System.out.println(loaded.getId() + " - " + loaded.getFirstName()));
+    }
+}
